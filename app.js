2cdcecf76cc6d16adee285c45dc2955b78ac0937
Restyled.io <commits@restyled.io>
2023-02-12 13:01:29 +0000

    Restyled by clang-format

--- app.js
+++ app.js
@@ -4,32 +4,25 @@ const app = express();
 
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
-    res.json({ status: "success", message: "Welcome To Testing API" });
+  res.json({status : "success", message : "Welcome To Testing API"});
 });
 
-app.get("/name", (req, res) => {
-    res.json({ status: "success", name: "Nwachukwu" });
-});
+app.get("/name",
+        (req, res) => { res.json({status : "success", name : "Nwachukwu"}); });
 
 app.post("/add", (req, res) => {
-    const { num1, num2 } = req.body;
-    const add = (num1, num2) => {
-        return num1 + num2;
-    };
-    res.json({
-        status: "success",
-        result: "Welcome To Testing API",
-        result: add(num1, num2)
-    });
+  const {num1, num2} = req.body;
+  const add = (num1, num2) => { return num1 + num2; };
+  res.json({
+    status : "success",
+    result : "Welcome To Testing API",
+    result : add(num1, num2)
+  });
 });
-
 
 const PORT = process.env.PORT || 3000;
 
-
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
-
 
 module.exports = app;
