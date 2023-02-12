cb54d7eb1a484bee7c80ad5b2c104c02b8ad2031
Restyled.io <commits@restyled.io>
2023-02-12 13:01:32 +0000

    Restyled by prettier

--- app.js
+++ app.js
@@ -5,19 +5,22 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({status : "success", message : "Welcome To Testing API"});
+  res.json({ status: "success", message: "Welcome To Testing API" });
 });
 
-app.get("/name",
-        (req, res) => { res.json({status : "success", name : "Nwachukwu"}); });
+app.get("/name", (req, res) => {
+  res.json({ status: "success", name: "Nwachukwu" });
+});
 
 app.post("/add", (req, res) => {
-  const {num1, num2} = req.body;
-  const add = (num1, num2) => { return num1 + num2; };
+  const { num1, num2 } = req.body;
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
   res.json({
-    status : "success",
-    result : "Welcome To Testing API",
-    result : add(num1, num2)
+    status: "success",
+    result: "Welcome To Testing API",
+    result: add(num1, num2),
   });
 });
 
